40f7fba6f152bfd5731035579bf7ff9dec45b1ab
UNIX228 <eugene100@vk.com>
2025-07-24 18:30:46 +0300

    NEOVIA v1.0.0

--- source/config.cpp
+++ source/config.cpp
@@ -1,11 +1,8 @@
 #include "config.h"
 #include "neovia.h"
 #include <switch.h>
-#include <fstream>
-#include <map>
 
 Result loadConfig(Config& config) {
-    // Пытаемся загрузить конфигурацию
     config.firstRun = true;
     config.language = LANG_RU;
     config.priority = PRIORITY_GRAPHICS;
@@ -13,36 +10,15 @@ Result loadConfig(Config& config) {
     config.downloadAllMods = true;
     config.autoStart = false;
     config.extrasInstalled = false;
-    
     return 0;
 }
 
 Result saveConfig(const Config& config) {
-    // Сохраняем конфигурацию
     return 0;
 }
 
 std::string getLanguageString(Language lang, const std::string& key) {
-    static std::map<std::string, std::map<Language, std::string>> translations = {
-        {"app_title", {
-            {LANG_RU, "NEOVIA"},
-            {LANG_EN, "NEOVIA"},
-            {LANG_UK, "NEOVIA"}
-        }},
-        {"settings", {
-            {LANG_RU, "Настройки"},
-            {LANG_EN, "Settings"},
-            {LANG_UK, "Налаштування"}
-        }}
-    };
-    
-    auto keyIt = translations.find(key);
-    if (keyIt != translations.end()) {
-        auto langIt = keyIt->second.find(lang);
-        if (langIt != keyIt->second.end()) {
-            return langIt->second;
-        }
-    }
-    
+    if (key == "app_title") return "NEOVIA";
+    if (key == "settings") return "Настройки";
     return key;
 }
