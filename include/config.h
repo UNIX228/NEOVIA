4e339edd6dd49814ec8a2f67f0600284a657e964
UNIX228 <eugene100@vk.com>
2025-07-24 17:52:48 +0300

    NEOVIA v1.0.0

---
+++ include/config.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "neovia.h"
+#include <switch.h>
+#include <string>
+#include <map>
+
+Result loadConfig(Config& config);
+Result saveConfig(const Config& config);
+std::string getLanguageString(Language lang, const std::string& key);
